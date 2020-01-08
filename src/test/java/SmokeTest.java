06005df9c54208b164439b1fff05c2e2415fc83b
ropio09 <rociors@live.com>
2020-01-08 17:03:19 -0500

    changed smokeTest

--- src/test/java/SmokeTest.java
+++ src/test/java/SmokeTest.java
@@ -1,10 +1,30 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class SmokeTest {
-    public static void main(String[] args) {
+    WebDriver driver;
+
+    @BeforeEach
+    public void setup() {
         System.setProperty("webdriver.chrome.driver", "resources/chromedriver.exe");
-        WebDriver driver= new ChromeDriver();
+        driver = new ChromeDriver();
         driver.get("https://amazon.com");
+    }
+
+    @Test
+    public void search() {
+    }
+
+    @Test
+    public void addToCart() {
+
+    }
+
+    public void terdown() {
+
+    }
+
     }
 }
